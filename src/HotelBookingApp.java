cd1144ee2b371597dbb2f265f878ad7c86ee7c17
Mithrangr <grmithran@gmail.com>
2026-03-30 14:17:42 +0530

    UC1:Initialize Train and Display Consist Summary

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,9 +1,14 @@
-public class HotelBookingApp {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Hotel Booking System");
-        System.out.println("Application: Hotel Booking System v1.0");
-        System.out.println("Application started successfully. Exiting now...");
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count: " + trainConsist.size());
     }
 }
