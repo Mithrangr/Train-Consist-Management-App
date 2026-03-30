d110f4ffc41eac49d94d00c21c0e4bfa17ed1564
Mithrangr <grmithran@gmail.com>
2026-03-30 14:03:07 +0530

    UC1:Application Entry & Welcome Message

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,9 @@
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Hotel Booking System");
+        System.out.println("Application: Hotel Booking System v1.0");
+        System.out.println("Application started successfully. Exiting now...");
+    }
+}
