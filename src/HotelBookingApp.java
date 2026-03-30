4cc671c1a4c0e2f4bf509774ea26efaef7bd2a2b
Mithrangr <grmithran@gmail.com>
2026-03-30 14:22:24 +0530

    UC2:Add Passenger Bogies to Train (ArrayList Operations)

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
-import java.util.List;
 
-public class TrainApp {
+public class TrainAppUC2 {
 
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
 
-        List<String> trainConsist = new ArrayList<>();
+        ArrayList<String> bogies = new ArrayList<>();
 
-        System.out.println("Initial Bogie Count: " + trainConsist.size());
+        bogies.add("Sleeper");
+        bogies.add("AC Chair");
+        bogies.add("First Class");
+
+        System.out.println("Bogies after insertion: " + bogies);
+
+        bogies.remove("AC Chair");
+
+        System.out.println("After removing AC Chair: " + bogies);
+
+        System.out.println("Does Sleeper exist? " + bogies.contains("Sleeper"));
+
+        System.out.println("Final Bogie List: " + bogies);
     }
 }
