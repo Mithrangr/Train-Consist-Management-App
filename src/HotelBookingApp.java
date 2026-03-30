04822fb4741e8de827cc41d3dbab0d353857bce4
Mithrangr <grmithran@gmail.com>
2026-03-30 15:10:49 +0530

    UC1: Add welcome message module

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 class Bogie {
     String name;
@@ -11,13 +9,9 @@ class Bogie {
         this.name = name;
         this.capacity = capacity;
     }
-
-    public String toString() {
-        return name + " (" + capacity + ")";
-    }
 }
 
-public class TrainAppUC9 {
+public class TrainAppUC10 {
 
     public static void main(String[] args) {
 
@@ -28,13 +22,11 @@ public class TrainAppUC9 {
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 60));
         bogies.add(new Bogie("First Class", 24));
-        bogies.add(new Bogie("Sleeper", 72));
 
-        Map<String, List<Bogie>> grouped = bogies.stream()
-                .collect(Collectors.groupingBy(b -> b.name));
+        int totalCapacity = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
 
-        for (Map.Entry<String, List<Bogie>> entry : grouped.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
+        System.out.println("Total Seating Capacity: " + totalCapacity);
     }
 }
