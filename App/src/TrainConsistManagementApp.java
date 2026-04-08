93627380b45dcf8d49d261762f3a3b8e6be0a496
Mithrangr <grmithran@gmail.com>
2026-04-08 14:57:05 +0530

    UC12:Safety Compliance Check for Goods Bogies

---
+++ App/src/TrainConsistManagementApp.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.function.Predicate;
+
+class Bogie {
+    String type;
+    String cargo;
+
+    Bogie(String type, String cargo) {
+        this.type = type;
+        this.cargo = cargo;
+    }
+}
+
+public class TrainSafetyApp {
+    public static void main(String[] args) {
+
+        List<Bogie> bogies = Arrays.asList(
+                new Bogie("Cylindrical", "Petroleum"),
+                new Bogie("Box", "Coal"),
+                new Bogie("Cylindrical", "Petroleum")
+        );
+
+        Predicate<Bogie> rule = b ->
+                !(b.type.equals("Cylindrical") && !b.cargo.equals("Petroleum"));
+
+        boolean isSafe = bogies.stream().allMatch(rule);
+
+        if (isSafe)
+            System.out.println("Train is Safety Compliant");
+        else
+            System.out.println("Train is NOT Safety Compliant");
+    }
+}
