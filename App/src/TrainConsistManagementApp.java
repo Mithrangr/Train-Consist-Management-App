32a6a260d5ab96314ca31e926cc13df5ec841128
Mithrangr <grmithran@gmail.com>
2026-04-08 16:04:50 +0530

    UC19:Binary Search for Bogie ID (Optimized Searching)

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LinearSearchApp {
+public class BinarySearchApp {
     public static void main(String[] args) {
 
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG523"};
@@ -9,12 +9,21 @@ public class LinearSearchApp {
         System.out.print("Enter Bogie ID to search: ");
         String key = sc.nextLine();
 
+        int low = 0, high = bogieIds.length - 1;
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(key)) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int res = key.compareTo(bogieIds[mid]);
+
+            if (res == 0) {
                 found = true;
                 break;
+            } else if (res < 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
